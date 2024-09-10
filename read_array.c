6aac5646597011751b32e3ae7b3ffc152e12ec31
Glenn James <jrg94@zoo.cs.yale.edu>
2024-09-10 11:11:52 -0400

    added total function

--- read_array.c
+++ read_array.c
@@ -1,20 +1,36 @@
 #include <stdio.h>
 
+/**
+ * Returns the total of the values in the given array.
+ *
+ * @param n a non-negative integer
+ * @param a an array with at least n elements
+ * @return the total of the first n elements of a
+ */
+double total_array(size_t n, double a[]);
+
 int main()
 {
-  double arr[6];
+  const size_t SIZE = 6;
+  double arr[SIZE];
 
-  for (size_t i = 0; i < 6; i++)
+  for (size_t i = 0; i < SIZE; i++)
     {
       fscanf(stdin, "%lf", &arr[i]);
     }
   
-  double total = 0.0;
-  for (size_t i = 0; i < 6; i++)
-    {
-      total += arr[i];
-    }
-  printf("Average: %lf\n", total / 6);
+  double total = total_array(SIZE, arr);
 
-  return 0;
+  printf("Average: %lf\n", total / SIZE);
+
+}
+
+double total_array(size_t n, double a[])
+{
+  double total = 0.0;
+  for (size_t i = 0; i < n; i++)
+    {
+      total += a[i];
+    }
+  return total;
 }
