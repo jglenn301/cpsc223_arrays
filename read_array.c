de83ad646994b2c97848d4a5baa478771852985c
Glenn James <jrg94@zoo.cs.yale.edu>
2024-09-10 11:13:08 -0400

    added read function

--- read_array.c
+++ read_array.c
@@ -1,6 +1,15 @@
 #include <stdio.h>
 
 /**
+ * Reads n space-separated doubles from standard input into the given
+ * array.
+ *
+ * @param n a non-negative integer
+ * @param a an array with at least n elements
+ */
+void read_array(size_t n, double a[]);
+
+/**
  * Returns the total of the values in the given array.
  *
  * @param n a non-negative integer
@@ -14,15 +23,21 @@ int main()
   const size_t SIZE = 6;
   double arr[SIZE];
 
-  for (size_t i = 0; i < SIZE; i++)
-    {
-      fscanf(stdin, "%lf", &arr[i]);
-    }
+  read_array(SIZE, arr);
   
   double total = total_array(SIZE, arr);
 
   printf("Average: %lf\n", total / SIZE);
 
+  return 0;
+}
+
+void read_array(size_t n, double a[])
+{
+  for (size_t i = 0; i < n; i++)
+    {
+      fscanf(stdin, "%lf", &a[i]);
+    }
 }
 
 double total_array(size_t n, double a[])
